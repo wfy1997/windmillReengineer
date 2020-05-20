21ce5ffa15f9b057e924be546c4ee95765986481
John Jacquay <john@bioteam.net>
2020-05-20 11:39:56 -0600

    Inital commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import DataDictionary from './DataDictionary/DataDictionary';
+
+function App() {
+  return (
+   <DataDictionary />
+  );
+}
+
+export default App;
