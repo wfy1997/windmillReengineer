2b0cfc918b743bbc2b0d77c486ceec871627c653
John Jacquay <john@bioteam.net>
2020-05-21 10:38:51 -0600

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import ReduxDataDictionary from './DataDictionary';
+import reducers from './reducers'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import dict from './dict';
+const version = {"commit":"913161064b02bcef024d072873e77c8c79cc1a68","dictionary":{"commit":"520a25999fd183f6c5b7ddef2980f3e839517da5","version":"0.2.1-9-g520a259"},"version":"4.0.0-44-g9131610"};
+
+async function init() {
+  const store = createStore(reducers);
+
+  await Promise.all(
+    [
+      store.dispatch({
+        type: 'RECEIVE_DICTIONARY',
+        data: dict
+      }),
+      store.dispatch({
+        type: 'RECEIVE_VERSION_INFO',
+        data: version
+      })
+    ],
+  );
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <ReduxDataDictionary />
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+
+init();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
